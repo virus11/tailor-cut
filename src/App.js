cfad9686135066bc5167cc49b433138ad710d63c
virus <virendra.rathod61@gmail.com>
2022-09-08 12:44:02 +0530

    Homepage improved

--- src/App.js
+++ src/App.js
@@ -14,10 +14,10 @@ function App() {
           <MDBCol md='6' className="mt-3">
             <MDBCard>
               <MDBRow>
-                <MDBCol size='3' className='d-flex align-items-center'>
-                  <MDBCardImage src='/images/tailor-customer-new.jpg' alt='...' fluid />
+                <MDBCol size='4' className='d-flex align-items-center'>
+                  <MDBCardImage src='/images/tailor-customer-new.png' alt='...' fluid />
                 </MDBCol>
-                <MDBCol size='9'>
+                <MDBCol size='8' className='d-flex align-items-center'>
                   <MDBCardBody>
                     <MDBCardTitle>Track your orders</MDBCardTitle>
                     <MDBBtn>Customer Login</MDBBtn>
@@ -29,19 +29,33 @@ function App() {
           <MDBCol md='6' className="mt-3">
             <MDBCard>
               <MDBRow>
-                <MDBCol size='3' className='d-flex align-items-center'>
-                  <MDBCardImage src='/images/tailor-activities-new.jpg' alt='...' fluid />
+                <MDBCol size='4' className='d-flex align-items-center'>
+                  <MDBCardImage src='/images/tailor-activities-new.png' alt='...' fluid />
                 </MDBCol>
-                <MDBCol size='9'>
+                <MDBCol size='8' className='d-flex align-items-center'>
                   <MDBCardBody>
-                    <MDBCardTitle>Manage your Tailoring Business for Free</MDBCardTitle>
-                    <MDBBtn>Login / Register</MDBBtn>
+                    <MDBCardTitle>Manage your Tailoring Shop</MDBCardTitle>
+                    <MDBBtn>Business Login</MDBBtn>
                   </MDBCardBody>
                 </MDBCol>
               </MDBRow>
             </MDBCard>
           </MDBCol>         
         </MDBRow>
+
+        <MDBRow className="mt-5">
+            
+                <MDBCol md='8' className='d-flex align-items-center'>
+                  <MDBCardBody>
+                    <MDBCardTitle>Manage your Tailoring Shop</MDBCardTitle>
+                    <MDBBtn>Business Login</MDBBtn>
+                  </MDBCardBody>
+                </MDBCol>
+                <MDBCol md='4' className='d-flex align-items-center'>
+                  <MDBCardImage src='/images/tailor-activities.jpg' alt='...' fluid />
+                </MDBCol>
+              </MDBRow> 
+
       </MDBContainer>
      
     </div>
