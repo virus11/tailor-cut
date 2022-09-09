a2c71cbcadb3f4ce42ec1bff34df2035a4a242a1
virus <virendra.rathod61@gmail.com>
2022-09-09 19:38:12 +0530

    Tailor Dashboard added

--- src/components/business/AdminDashboard.js
+++ src/components/business/AdminDashboard.js
@@ -1,5 +1,4 @@
 import * as React from 'react';
-import PropTypes from 'prop-types';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import CssBaseline from '@mui/material/CssBaseline';
@@ -16,6 +15,52 @@ import MailIcon from '@mui/icons-material/Mail';
 import MenuIcon from '@mui/icons-material/Menu';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
+import SearchIcon from '@mui/icons-material/Search';
+
+import { styled, alpha } from '@mui/material/styles';
+import InputBase from '@mui/material/InputBase';
+
+const Search = styled('div')(({ theme }) => ({
+  position: 'relative',
+  borderRadius: theme.shape.borderRadius,
+  backgroundColor: alpha(theme.palette.common.white, 0.15),
+  '&:hover': {
+    backgroundColor: alpha(theme.palette.common.white, 0.25),
+  },
+  marginLeft: 0,
+  width: '100%',
+  [theme.breakpoints.up('sm')]: {
+    marginLeft: theme.spacing(1),
+    width: 'auto',
+  },
+}));
+
+const SearchIconWrapper = styled('div')(({ theme }) => ({
+  padding: theme.spacing(0, 2),
+  height: '100%',
+  position: 'absolute',
+  pointerEvents: 'none',
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+}));
+
+const StyledInputBase = styled(InputBase)(({ theme }) => ({
+  color: 'inherit',
+  '& .MuiInputBase-input': {
+    padding: theme.spacing(1, 1, 1, 0),
+    // vertical padding + font size from searchIcon
+    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+    transition: theme.transitions.create('width'),
+    width: '100%',
+    [theme.breakpoints.up('sm')]: {
+      width: '12ch',
+      '&:focus': {
+        width: '20ch',
+      },
+    },
+  },
+}));
 
 const drawerWidth = 240;
 
@@ -67,10 +112,7 @@ function AdminDashboard(props){
       <CssBaseline />
       <AppBar
         position="fixed"
-        sx={{
-          width: { sm: `calc(100% - ${drawerWidth}px)` },
-          ml: { sm: `${drawerWidth}px` },
-        }}
+        sx={{ zIndex: (theme) => theme.zIndex.drawer + 1 }}
       >
         <Toolbar>
           <IconButton
@@ -83,9 +125,19 @@ function AdminDashboard(props){
             <MenuIcon />
           </IconButton>
           <Typography variant="h6" noWrap component="div">
-            Responsive drawer
+            Tailor Cut
           </Typography>
+          <Search className="mx-auto w-50">
+            <SearchIconWrapper>
+              <SearchIcon />
+            </SearchIconWrapper>
+            <StyledInputBase
+              placeholder="Search…"
+              inputProps={{ 'aria-label': 'search' }}
+            />
+          </Search>
         </Toolbar>
+         
       </AppBar>
       <Box
         component="nav"
--- src/components/business/HomepageBusiness.js
+++ src/components/business/HomepageBusiness.js
@@ -1,15 +1,26 @@
-import { MDBContainer } from "mdb-react-ui-kit";
+import { MDBContainer, MDBInput, MDBRow, MDBCol, MDBCardBody, MDBCardImage } from 'mdb-react-ui-kit'
 import { Link } from "react-router-dom";
+import Typography from '@mui/material/Typography';
 
 function HomepageBusiness(){
 
     return (
-        <div>
             <MDBContainer  className="mt-5 pt-3">
-         <Link to="/admin-dashboard">Admin Dashboard</Link>
-         </MDBContainer>
-        </div>
-    );
+                <MDBRow className="mt-3 d-flex align-items-center text-center">                        
+                    <MDBCol md='6' className=''>
+                        <MDBCardBody>
+                            <h3>Welcome</h3>
+                            <Typography variant="h3" gutterBottom className="text-primary">Trimurty Tailors</Typography>
+                            <MDBInput label='4 digit PIN' id='typeMobileNumber' type='number' className='mt-3'/>
+                            <Link to="/admin-dashboard" className="mt-3 btn btn-primary">Enter</Link>                    
+                        </MDBCardBody>
+                    </MDBCol>
+                    <MDBCol md='6' className=''>
+                        <MDBCardImage src='/images/enter-pin.png' alt='...' fluid />
+                    </MDBCol>
+                </MDBRow> 
+            </MDBContainer>
+        );
 }
 
 export default HomepageBusiness;
